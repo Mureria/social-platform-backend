ee5242a19a8d389dbc3bd5c1a2962d62a2553c49
Mureria <chosenireri42@gmail.com>
2023-07-30 11:01:37 +0300

    second cd

--- server.js
+++ server.js
@@ -1,30 +1,17 @@
-require('dotenv').config();
-
-const dbUrl = process.env.DB_URL;
-
-const myName = 'myName'
+const dotenv= require("dotenv");
+dotenv.config()
 
 const express = require ('express')
 const mongoose = require ('mongoose')
-
-const app = express();
+const app= express();
 const PORT = 3000;
 
-mongoose.connect( dbUrl, {
-    useNewUrlParser : true,
-    useUnifiedTopology :true, 
-
-
+mongoose.connect(
+  process.env.DB_URL
+)
+.then(console.log("connected to mongo"))
+.catch((err)=>console.log(err))
+app.listen(PORT,()=>{
+  console.log("Backend is running");
 });
 
-const db = mongoose.connection;
-
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => {
-    console.log('Connected to the db');
-  });
-
-  app.listen(PORT, () => {
-    console.log(`server is running on port ${PORT}`);
-
-  });
